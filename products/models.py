bf66162ea9216876408dba990eaacb2e343359b4
Laurie Anderson <laurieanderson92@hotmail.co.uk>
2024-12-08 17:25:57 +0000

    build: install allauth, create product models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+# category class taken from boutique ado
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    def get_friendly_name(self):
+        return self.friendly_name
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    dimension = models.TextField()
+    cost = models.DecimalField(max_digits=6, decimal_places=2)
+    craft_time = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(40)])
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
