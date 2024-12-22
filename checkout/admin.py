97d553cf64f8435a365f48500b869c9e0e34b75b
Laurie Anderson <laurieanderson92@hotmail.co.uk>
2024-12-22 22:30:59 +0000

    refactor: add admin class for order

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Order
+
+class OrderAdmin(admin.ModelAdmin):
+    readonly_fields = ('order_number','customer','product','date','delivery_cost','order_cost','grand_total')
+
+    ordering = ['-date']
+
+    list_display = ('order_number','date','full_name','grand_total')
+
 
 # Register your models here.
+admin.site.register(Order, OrderAdmin)
