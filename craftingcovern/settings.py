011b9baa03d5e29c0028f9f199521c84419806fa
Laurie Anderson <laurieanderson92@hotmail.co.uk>
2025-01-21 21:10:55 +0000

    chore: set TLS to True

--- craftingcovern/settings.py
+++ craftingcovern/settings.py
@@ -198,7 +198,7 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 
 # Email Configs
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = False
+EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
